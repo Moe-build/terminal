c58c66f6f39c3752503fbf968ca47d9b82abb170
GunMoe <i@dounait.cn>
2020-09-13 12:21:31 +0800

    windows terminal init

---
+++ windows terminal/initial.ps1
@@ -0,0 +1,8 @@
+
+mkdir "$env:LOCALAPPDATA\terminal"
+Copy-Item -Path .\icon -Destination $env:LOCALAPPDATA\terminal  -Recurse
+
+$terminal_text = Get-Content ".\terminal.reg"
+$terminal_text.Replace("[user_name]",$env:UserName) | Out-File ./_terminal.reg
+
+

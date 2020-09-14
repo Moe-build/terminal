6d3075895c034f7e4070611480f8150c6383d7b6
GunMoe <i@dounait.cn>
2020-09-14 13:35:09 +0800

    .

--- windows terminal/initial.ps1
+++ windows terminal/initial.ps1
@@ -1,6 +1,6 @@
 
 mkdir "$env:LOCALAPPDATA\terminal"
-Copy-Item -Path .\icon -Destination $env:LOCALAPPDATA\terminal  -Recurse
+Copy-Item -Path .\img -Destination $env:LOCALAPPDATA\terminal  -Recurse
 
 $terminal_text = Get-Content ".\terminal.reg"
 $terminal_text.Replace("[user_name]",$env:UserName) | Out-File ./_terminal.reg

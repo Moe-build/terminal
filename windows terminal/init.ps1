b26ee2e81e98f55923c83f87532a7b798ddcbbff
GunMoe <i@dounait.cn>
2021-01-23 22:00:23 +0800

    update

---
+++ windows terminal/init.ps1
@@ -0,0 +1,23 @@
+# init copy static file
+IF(Test-Path "$env:USERPROFILE\.moe\terminal")
+{
+    Remove-Item -force -recurse "$env:USERPROFILE\.moe\terminal"
+}
+mkdir "$env:USERPROFILE\.moe\terminal"
+Copy-Item -Path .\img\* -Destination $env:USERPROFILE\.moe\terminal  -Recurse
+
+# replace windows terminal setting 
+Copy-Item .\settings.json "$env:LOCALAPPDATA\Microsoft\Windows Terminal"
+
+# add right click
+#reg import .\terminal.reg
+New-Item -Force -Path Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\wt
+New-Item -Force -Path Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command
+New-ItemProperty -Force -Path Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\wt "(default)"  -value "Terminal here" -PropertyType string
+New-ItemProperty -Force -Path Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\wt "Icon"  -value "%USERPROFILE%\.moe\terminal\wt_32.ico" -PropertyType string
+New-ItemProperty -Force -Path Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command "(default)"  -value "D:\\Applications\\Scoop\\shims\\wt.exe"  -PropertyType string
+
+  
+
+
+
--- windows terminal/right-click.ps1
+++
@@ -1,4 +0,0 @@
-$terminal_text = Get-Content ".\terminal.reg"
-$terminal_text.Replace("[user_name]",$env:UserName) | Out-File ./_terminal.reg
-reg import ./_terminal.reg
-rm ./_terminal.reg
